77ac09036944907dc98c16d004abf580c04e02ec
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2020-01-10 16:46:34 +0000

    Clean Master

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import React, { useState } from "react";
-import { Person } from './components/Person'
+import React, { } from "react";
 import './App.css';
 
 function App() {
-  const [listName, setListName] = useState([
-    {
-      name: "Julia",
-      age: 26,
-    },
-    {
-      name: "Katlin",
-      age: 28,
-    },
-    {
-      name: "Athena",
-      age: 29,
-    },
-  ]);
-
-  const deleteElement = (index) => {
-    const newList = [...listName];
-    newList.splice(index, 1)
-    setListName(newList);
-  }
-
   return (
     <div className="App">
-      {listName.map((value, index) => (
-        <Person
-          key={value + index}
-          name={value.name}
-          age={value.age}
-          index={index}
-          deleteElement={deleteElement}
-        />
-      ))}
+      
     </div>
   );
 }
