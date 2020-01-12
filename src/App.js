5aede7ab73d66eef50e24af2a3105f16ee5c2b7f
Leonardo Vivanco <leonardovivanco@MBP-de-Leonardo.lan>
2020-01-12 20:05:57 +0000

    Eject files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { } from "react";
-import './App.css';
+import classes from './App.scss';
 
 function App() {
+  console.log(classes)
   return (
     <div className="App">
-      
+      <button className={classes.Button} >Button</button>
     </div>
   );
 }
